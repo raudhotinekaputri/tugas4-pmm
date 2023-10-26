f2770c67250157a188311a32bc50ccc48221a068
raudhotinekaputri <raudhotinekaputri@gmail.com>
2023-10-26 10:25:03 +0700

    update api

--- lib/helpers/api_url.dart
+++ lib/helpers/api_url.dart
@@ -1,20 +1,21 @@
 class ApiUrl {
-  static const String baseUrl = 'http://localhost/toko-api/public';
+  static const String baseUrl = 'http://10.100.2.162/toko-api/public';
 
-  static const String registrasi = baseUrl + '/registrasi';
-  static const String login = baseUrl + '/login';
-  static const String listProduk = baseUrl + '/produk';
-  static const String createProduk = baseUrl + '/produk';
+  static const String registrasi = '$baseUrl/register';
+  static const String login = '$baseUrl/login';
+  static const String listProduk = '$baseUrl/produk';
+  static const String createProduk = '$baseUrl/produk';
 
   static String updateProduk(int id) {
-    return baseUrl + '/produk/' + id.toString() + '/update';
+    return '$baseUrl/produk/$id';
   }
 
   static String showProduk(int id) {
-    return baseUrl + '/produk/' + id.toString();
+    return '$baseUrl/produk/$id';
   }
 
   static String deleteProduk(int id) {
-    return baseUrl + '/produk/' + id.toString();
+    return '$baseUrl/produk/$id';
   }
 }
+
